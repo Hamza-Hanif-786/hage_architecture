60dbdf919d5750548027307313dfec65eda8c4e3
Hamza Hanif <hamzahanif1024@gmail.com>
2025-04-08 14:22:45 +0500

    feat: add HeroSection component with responsive design and hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+import HeroSection from './sections/homepage/HeroSection';
 
 
 export default function Home() {
   return (
     <>
-      
+      <HeroSection />
     </>
   );
 }
