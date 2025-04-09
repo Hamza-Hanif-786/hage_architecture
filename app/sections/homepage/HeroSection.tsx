2af9464fb1b7d5237f8248d0060245e0f429481a
Hamza Hanif <hamzahanif1024@gmail.com>
2025-04-09 20:15:16 +0500

    feat: adjust HeroSection heading size for improved typography

--- app/sections/homepage/HeroSection.tsx
+++ app/sections/homepage/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <div className='px-8 py-24 inline-flex flex-col justify-start items-start gap-7 xl:gap-10'>
         <div className='flex flex-col justify-start items-start gap-4 sm:gap-6 md:gap-7 xl:gap-10'>
-            <h4 className="uppercase text-xl lg:text-2xl font-normal font-['Satoshi'] text-[#E44F39]">/01 Index</h4>
+            <h4 className="uppercase text-lg md:text-xl lg:text-2xl font-normal font-['Satoshi'] text-[#E44F39]">/01 Index</h4>
             <h1 className="uppercase text-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl font-bold font-['Satoshi'] leading-10 md:leading-14 lg:leading-[4.5rem] xl:leading-28">
                 Architects that design <span className='text-[#E44F39]'> for future</span>.
             </h1>
