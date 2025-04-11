8945064c75f7b1516e87c7eecd0e2882f707a947
Hamza Hanif <hamzahanif1024@gmail.com>
2025-04-11 13:32:40 +0500

    feat: enhance ProjectSection with ProjectCard components and add project images

--- app/sections/homepage/ProjectSection.tsx
+++ app/sections/homepage/ProjectSection.tsx
@@ -1,5 +1,10 @@
+import ProjectCard from '@/app/components/ProjectCard'
 import Link from 'next/link'
 import React from 'react'
+import project1pic from '@/public/images/project1.webp'
+import project2pic from '@/public/images/project2.webp'
+import project3pic from '@/public/images/project3.webp'
+import project4pic from '@/public/images/project4.webp'
 
 const ProjectSection = () => {
   return (
@@ -12,7 +17,7 @@ const ProjectSection = () => {
                         More Project
                     </Link>
                 </div>
-                <div className='w-full md:w-1/2'>
+                <div className='w-full lg:w-1/2'>
                     <p className="text-black text-lg sm:text-xl font-normal font-['Satoshi']">
                         Explore the breadth and depth of our design expertise through a curated selection of our standout projects. From residential havens to commercial landmarks, each project showcases our commitment to excellence, innovation, and client satisfaction.
                     </p>
@@ -20,7 +25,42 @@ const ProjectSection = () => {
             </div>
 
         
-            <div></div>
+            <div>
+                <ProjectCard
+                    title="Urban Oasis" 
+                    description="A modern apartment designed for urban living, featuring sleek aesthetics and smart"
+                    imageUrl={project1pic.src} 
+                    city="Jakarta"
+                    year="2023"
+                    type="Residential"
+                />
+                <ProjectCard
+                    title="Coastal Retreat" 
+                    description="A beachfront villa that seamlessly blends indoor and outdoor living, capturing the essence of coastal luxury."
+                    imageUrl={project2pic.src} 
+                    city="Bali"
+                    year="2022"
+                    type="Residential"
+                    direction='rtl'
+                />
+                <ProjectCard
+                    title="Corporate Hub" 
+                    description="A contemporary office space designed to foster collaboration and productivity among employees, with an emphasis on modern amenities and flexible workspaces."
+                    imageUrl={project3pic.src} 
+                    city="Surabaya"
+                    year="2021"
+                    type="Commercial"
+                />
+                <ProjectCard
+                    title="Boutique Bistro" 
+                    description="An intimate restaurant nestled in a historic district, exuding charm and sophistication while offering a memorable dining experience."
+                    imageUrl={project4pic.src} 
+                    city="Yogyakarta"
+                    year="2024"
+                    type="Architectural"
+                    direction='rtl'
+                />
+            </div>
         </div>
     </div>
   )
