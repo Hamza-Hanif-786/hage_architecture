3aaa2a896f782f69421e8b9f0fceb17a206a2620
Hamza Hanif <hamzahanif1024@gmail.com>
2025-04-13 13:10:22 +0500

    fix: update ServiceSection to use theme variable 'carot' for header color and add visual indicator for service links

--- app/sections/homepage/ServiceSection.tsx
+++ app/sections/homepage/ServiceSection.tsx
@@ -21,7 +21,7 @@ const ServiceSection = () => {
             </div>
             <div className='flex flex-col justify-between h-full gap-20 lg:gap-0'>
                 <div className='flex flex-col gap-5 md:gap-5 lg:gap-9 xl:gap-11'>
-                    <h4 className="uppercase text-lg md:text-xl lg:text-2xl font-normal font-['Satoshi'] text-[#E44F39]">/03 Our Service</h4>
+                    <h4 className="uppercase text-lg md:text-xl lg:text-2xl font-normal font-['Satoshi'] text-carot">/03 Our Service</h4>
                     <p className="text-black text-lg lg:text-xl font-normal font-['Satoshi']">
                         At Hage, we offer a comprehensive range of services aimed at fulfilling your design needs and exceeding your expectations. Explore our array of offerings tailored to bring your vision to life with creativity, expertise, and attention to detail.
                     </p>
@@ -30,7 +30,10 @@ const ServiceSection = () => {
                     <ul className='space-y-2 lg:space-y-4'>
                         {servicelinks.map((service, index) => (
                             <Link href={service.link} key={index} className='group cursor-pointer flex items-center justify-between border-neutral-500 border-b pb-3 xl:pb-5 text-black text-xl xl:text-2xl font-normal font-["Satoshi"]'>
-                                {service.name}
+                                <div className='flex items-center gap-4'>
+                                    <div className='size-3 bg-carot rounded-full' />
+                                    {service.name}
+                                </div>
                                 <ChevronsRight className='w-6 h-6 group-hover:translate-x-1 duration-300 ease-in' />
                             </Link>
                         ))}
