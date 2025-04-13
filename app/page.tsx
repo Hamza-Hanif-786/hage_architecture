5cf9b1077df66db0f5fd3e4ec7fa648a3e6ba35d
Hamza Hanif <hamzahanif1024@gmail.com>
2025-04-13 10:17:39 +0500

    feat: add BlogSection component to display blog posts with images and descriptions

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import AboutSection from './sections/homepage/AboutSection';
 import ServiceSection from './sections/homepage/ServiceSection';
 import ProjectSection from './sections/homepage/ProjectSection';
 import TestimonialSection from './sections/homepage/TestimonialSection';
+import BlogSection from './sections/homepage/BlogSection';
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <ServiceSection />
       <ProjectSection />
       <TestimonialSection />
+      <BlogSection />
     </>
   );
 }
---
+++ app/sections/homepage/BlogSection.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link'
+import React from 'react'
+import blog1 from '@/public/images/blog1.webp'
+import blog7 from '@/public/images/blog7.webp'
+import BlogCard from '@/app/components/BlogCard'
+
+const BlogSection = () => {
+    const blogs = [
+        {
+            id: 1,
+            image: blog1,
+            title: 'Design Tips for Maximizing Your Square Footage',
+            date: 'May 20, 2023',
+            description: 'Dive into our guide on making the most of compact living spaces, with tips on clever storage solutions, multi-functional furniture, and space-saving design hacks.',
+            link: '/blogs/design-tips-for-maximizing-your-square-footage'
+        },
+        {
+            id: 2,
+            image: blog7,
+            title: 'Building a Greener Future, One Project at a Time',
+            date: 'Jan 28, 2024',
+            description: 'Join us as we dive into the world of sustainable design practices, from eco-friendly materials to energy-efficient technologies, and discover how you can make a positive impact on the environment through your design choices.',
+            link: '/blogs/building-a-greener-future-one-project-at-a-time'
+        },
+    ];
+
+  return (
+    <div className='px-8 py-24 bg-black'>
+        <div className='flex flex-col gap-11'>
+            <div className='flex flex-col gap-6 sm:gap-9 md:gap-11'>
+                <div className='flex flex-row items-center justify-between w-full gap-2'>
+                    <h4 className="uppercase text-lg md:text-xl lg:text-2xl font-normal font-['Satoshi'] text-[#E44F39]">/06 Explore Our Blogs</h4>
+                    <Link className="uppercase bg-[#E44F39] px-2.5 py-2 md:px-4 md:py-3 text-sm sm:text-base font-medium text-white font-['Inter'] rounded-none hover:scale-105 transition-all duration-300 ease-in" href="/blogs">
+                        More Blogs
+                    </Link>
+                </div>
+                <p className="text-white text-lg sm:text-xl font-normal font-['Satoshi'] w-full lg:w-1/2">
+                    Welcome to the Hage Blog, your go-to destination for inspiration, insights, and expert advice on all things interior design, architecture, and beyond. Discover a treasure trove of articles, tips, and trends curated by our team of design enthusiasts and industry experts.
+                </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                {blogs.map((blog) => (
+                    <BlogCard key={blog.id}
+                        image={blog.image.src}
+                        title={blog.title}
+                        date={blog.date}
+                        description={blog.description}
+                        link={blog.link}
+                    />
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BlogSection
