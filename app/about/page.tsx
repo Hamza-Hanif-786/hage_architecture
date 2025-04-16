ac8e4e4de2267fff5e279fd796da0ee613afb211
Hamza Hanif <hamzahanif1024@gmail.com>
2025-04-16 08:54:49 +0500

    feat: add JoinConversationSection to the About page for enhanced user engagement

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import HeroSection from '../sections/about/HeroSection'
 import AboutSection from '../sections/about/AboutSection'
+import JoinConversationSection from '../sections/JoinConversationSection'
 
 const page = () => {
   return (
     <>
         <HeroSection />
         <AboutSection />
+        
+        <JoinConversationSection />
     </>
   )
 }
