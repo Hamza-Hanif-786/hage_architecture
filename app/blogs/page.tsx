190356f0e66e89f6ca260d2ef2a0f0b43cd2d4cb
Hamza Hanif <hamzahanif1024@gmail.com>
2025-04-17 09:55:52 +0500

    fix: remove trailing periods from headings in Blog and HeroSection components

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -64,7 +64,7 @@ const page = () => {
         <div className='px-8 py-24 inline-flex flex-col justify-start items-start gap-7 xl:gap-11'>
             <div className='flex flex-col justify-start items-start gap-4 sm:gap-6 md:gap-7 xl:gap-10'>
                 <h1 className="uppercase text-black text-5xl sm:text-5xl md:text-[4rem] lg:text-[5.5rem] xl:text-9xl font-bold font-['Satoshi'] leading-12 md:leading-14 lg:leading-24 xl:leading-28">
-                    Explore <span className='text-carot'> Our Blog</span>.
+                    Explore <span className='text-carot'> Our Blog</span>
                 </h1>
                 <p className="text-black font-normal text-base md:text-lg lg:text-xl font-['Satoshi'] md:w-[90%] lg:w-1/2">
                     Welcome to the Hage Blog, your ultimate source of inspiration, insights, and expert advice on all things design, architecture, and lifestyle. 
--- app/sections/about/HeroSection.tsx
+++ app/sections/about/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
     <div className='px-8 py-24 inline-flex flex-col justify-start items-start gap-7 xl:gap-10'>
         <div className='flex flex-col justify-start items-start gap-4 sm:gap-6 md:gap-7 xl:gap-10'>
             <h1 className="uppercase text-black text-5xl sm:text-5xl md:text-[4rem] lg:text-[5.5rem] xl:text-9xl font-bold font-['Satoshi'] leading-12 md:leading-14 lg:leading-24 xl:leading-28">
-                Welcome to <span className='text-carot'> Hage</span>.
+                Welcome to <span className='text-carot'> Hage</span>
             </h1>
             <p className="text-black font-normal text-base md:text-lg lg:text-xl font-['Satoshi'] md:w-[90%] lg:w-1/2">
                 We are Hage, a passionate team inspired by the power of design to transform the world around us. 
