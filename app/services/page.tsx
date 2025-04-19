67d4e7cb900f0e3a19d32471e8ba573b1bacd1a3
Hamza Hanif <hamzahanif1024@gmail.com>
2025-04-19 20:28:14 +0500

    feat: add HeroSection component for services page and update import path

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HeroSection from '../sections/services/HeroSection'
+import HeroSection from '../sections/servicespage/HeroSection'
 
 const page = () => {
   return (
